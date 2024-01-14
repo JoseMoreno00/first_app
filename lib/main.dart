263897b35b9ba197dd874f724451916fdaec6dd7
JoseMoreno00 <accgitjosem@gmail.com>
2024-01-14 20:16:25 -0300

    Player of the App (concept design)

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("I am Rich"),
-          centerTitle: true,
+          title: Image(
+            image: AssetImage('assets/icono.png'),
+          ),
+          backgroundColor: Colors.orange[800],
           foregroundColor: Colors.white,
-          backgroundColor: Colors.blueGrey[900],
         ),
-        backgroundColor: Colors.blueGrey[100],
+        backgroundColor: Colors.brown[100],
         body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://elcomercio.pe/resizer/AB93Kg1JoITGLMLkCgLBnVzg_7g=/980x528/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/37OWRM2CLBAE7BP5SXKLVMNHZE.jpg'),
+          child: Padding(
+            padding: const EdgeInsets.all(11.0),
+            child: Column(
+              children: [
+                Image(
+                    image: NetworkImage(
+                        'https://3.bp.blogspot.com/-6AseAmRZrbU/XIb1LLbWPfI/AAAAAAAAByU/J0-_7ErMcKQteJVQGMcuklbuXPY3UhweQCLcBGAs/s1600/tres-cerditos-lampara-maravillosa.png')),
+                Text(
+                    "Será cierto lo que se cuenta, que este cuento ni pretende ni aparenta, pero a mí me lo narraron de forma que se entienda, que aunque ni rima ni resalta: es la verdad científica exacta. La historia es sobre tres cerditos que vivían en el campo. Todos hermanables y unidos. Mutuamente apoyaban sus labores camperas. Su casita siempre limpia y ordenada los enorgullecía completamente y debiera. Pues muy bien complementaban.Un puerquito era inventor y construía lo que necesitaban; desde zapatitos para pezuñas hasta camisas llevaban. Otro cochinito era cocinero y por tanto, cocinaba; esas cosas ricas que a todos gustaban. Y por último, un cerdito escritor, quién redactaba inventos y recetas, pero de vez en cuando, algún cuento en su momento de poeta"),
+              ],
+            ),
           ),
         ),
       ),
     ),
   );
 }
-
